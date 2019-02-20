a53f7d02c8d7961867f6bfaa474f7aef872dfe2f
Sandeep SK <sandeep.sk@zoftsolutions.com>
2019-02-20 10:37:12 +0530

    write to csv

---
+++ app/model/write-to-csv-model.js
@@ -0,0 +1,60 @@
+'user strict';
+
+var Request = require("request");
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+var csvWriter = require('csv-write-stream')
+const dateFormat = require('dateformat');
+
+
+
+var WriteToCSV = function(WriteToCSV){
+    this.WriteToCSV=WriteToCSV;
+}
+
+
+
+
+WriteToCSV.write_to_csv = function allServices(custId,result) {
+    Request.get({
+        "headers": {
+
+            "Authorization":globalString,
+
+            "Content-Type": "application/json"
+        },
+        "url": "https://api.stripe.com/v1/charges?customer="+custId+"&&limit=1"
+    }, (error, response, body) => {
+        if (error) {
+            return console.dir(error);
+        }
+      // return console.dir(JSON.parse(response));
+      
+     
+      //var json=JSON.parse(body);
+      let json = JSON.parse(body);
+      var data=json.data;
+
+      let paymentID = data[0].id; 
+      let amount = data[0].amount; 
+      let created = data[0].created; 
+     
+      
+      var writer = csvWriter({sendHeaders: false})
+      writer.pipe(fs.createWriteStream(__dirname+ '/subscription-details.csv', {flags: 'a'}))
+      writer.write({a:custId, b:paymentID,c:amount,d:dateFormat(created*1000, "dd-mm-yyyy" ),e:dateFormat(created*1000, "dd-mm-yyyy" )})
+      writer.end()
+       result(null, body);
+        
+    });
+
+
+  
+}
+
+
+
+
+
+module.exports = WriteToCSV;
